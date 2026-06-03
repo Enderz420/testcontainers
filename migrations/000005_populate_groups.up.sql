7b0a33e4cf23f650ede642ef86df021894cb4ed2
Enderz420 <alice.m.risvik@gmail.com>
2026-06-03 08:39:57 +0200

    refactor: update names in groups

--- migrations/000005_populate_groups.up.sql
+++ migrations/000005_populate_groups.up.sql
@@ -2,7 +2,7 @@ IF NOT EXISTS (SELECT 1 FROM core.groups)
 BEGIN
     INSERT INTO core.groups (id, name, created_at, last_modified)
     VALUES
-        (NEWID(), 'Admin',  GETDATE(), GETDATE()),
-        (NEWID(), 'Editor', GETDATE(), GETDATE()),
-        (NEWID(), 'Viewer', GETDATE(), GETDATE());
+        (NEWID(), 'admin',  GETDATE(), GETDATE()),
+        (NEWID(), 'moderator', GETDATE(), GETDATE()),
+        (NEWID(), 'user', GETDATE(), GETDATE());
 END
