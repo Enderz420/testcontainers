cc4e56755cbd1b0563f928c3b8372464f152381b
Enderz420 <alice.m.risvik@gmail.com>
2026-06-03 10:28:49 +0200

    refactor: remove permission table and file

--- internal/data/models.go
+++ internal/data/models.go
@@ -6,17 +6,15 @@ import (
 )
 
 type Models struct {
-	Users       UserModel
-	Blogpost    BlogpostModel
-	Groups      GroupModel
-	Permissions PermissionModel
+	Users    UserModel
+	Blogpost BlogpostModel
+	Groups   GroupModel
 }
 
 func NewModels(db *sql.DB, timeout *time.Duration) Models {
 	return Models{
-		Users:       UserModel{DB: db, Timeout: timeout},
-		Blogpost:    BlogpostModel{DB: db, Timeout: timeout},
-		Groups:      GroupModel{DB: db, Timeout: timeout},
-		Permissions: PermissionModel{DB: db, Timeout: timeout},
+		Users:    UserModel{DB: db, Timeout: timeout},
+		Blogpost: BlogpostModel{DB: db, Timeout: timeout},
+		Groups:   GroupModel{DB: db, Timeout: timeout},
 	}
 }
--- migrations/000007_create_permissions_table.down.sql
+++
@@ -1,4 +0,0 @@
-IF OBJECT_ID('core.permissions', 'U') IS NOT NULL
-BEGIN
-    DROP TABLE core.permissions;
-END
