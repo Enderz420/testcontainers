3653a7c36fa8d54dc5583c28d9235a7b86851543
Enderz420 <alice.m.risvik@gmail.com>
2026-06-03 10:10:52 +0200

    feat: add permissions in data layer

--- internal/data/models.go
+++ internal/data/models.go
@@ -6,15 +6,17 @@ import (
 )
 
 type Models struct {
-	Users    UserModel
-	Blogpost BlogpostModel
-	Groups   GroupModel
+	Users       UserModel
+	Blogpost    BlogpostModel
+	Groups      GroupModel
+	Permissions PermissionModel
 }
 
 func NewModels(db *sql.DB, timeout *time.Duration) Models {
 	return Models{
-		Users:    UserModel{DB: db, Timeout: timeout},
-		Blogpost: BlogpostModel{DB: db, Timeout: timeout},
-		Groups:   GroupModel{DB: db, Timeout: timeout},
+		Users:       UserModel{DB: db, Timeout: timeout},
+		Blogpost:    BlogpostModel{DB: db, Timeout: timeout},
+		Groups:      GroupModel{DB: db, Timeout: timeout},
+		Permissions: PermissionModel{DB: db, Timeout: timeout},
 	}
 }
