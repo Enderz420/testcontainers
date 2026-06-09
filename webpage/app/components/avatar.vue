351c7e48cb1d68f1ed5a6cf635dd63da80623cc7
Enderz420 <alice.m.risvik@gmail.com>
2026-06-09 12:38:52 +0200

    feat: implement avatar

---
+++ webpage/app/components/avatar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  image?: string;
+  name: string;
+}>();
+</script>
+<template>
+  <div class="p-5 border-2 rounded-2xl text-black bg-pink-300">
+    <img v-if="image" :src="props.image" />
+    <span v-else> {{ props.name }}</span>
+  </div>
+</template>
